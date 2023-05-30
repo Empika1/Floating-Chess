3fbbf6fea3162bb620d399fb878970ec90e9321c
Empika1 <104784348+Empika1@users.noreply.github.com>
2023-05-30 14:15:07 -0400

    CHEES

--- Floating Chess/src/Pieces/Queen.java
+++ Floating Chess/src/Pieces/Queen.java
@@ -13,12 +13,29 @@ public final class Queen extends Piece {
         return pieceName;
     }
 
+    Rook fakeRook = new Rook();
+    Bishop fakeBishop = new Bishop();
     public boolean canMoveTo(Vector2I pos, ArrayList<Piece> whitePieces, ArrayList<Piece> blackPieces) {
-        return true;
+        fakeRook.setTruePos(getTruePos(), false);
+        fakeRook.setColor(getColor());
+        fakeBishop.setTruePos(getTruePos(), false);
+        fakeBishop.setColor(getColor());
+        return fakeRook.canMoveTo(pos, whitePieces, blackPieces) || fakeBishop.canMoveTo(pos, whitePieces, blackPieces);
     }
 
     public Vector2I closestValidPoint(Vector2I pos, ArrayList<Piece> whitePieces, ArrayList<Piece> blackPieces) {
-        return new Vector2I(0, 0);
+        fakeRook.setTruePos(getTruePos(), false);
+        fakeRook.setColor(getColor());
+        fakeBishop.setTruePos(getTruePos(), false);
+        fakeBishop.setColor(getColor());
+        Vector2I rookPoint = fakeRook.closestValidPoint(pos, whitePieces, blackPieces);
+        double rookSquaredDistance = pos.subtract(rookPoint).getSquaredLength();
+        Vector2I bishopPoint = fakeBishop.closestValidPoint(pos, whitePieces, blackPieces);
+        double bishopSquaredDistance = pos.subtract(bishopPoint).getSquaredLength();
+        if(rookSquaredDistance < bishopSquaredDistance)
+            return rookPoint;
+        else
+            return bishopPoint;
     }
 
     static final int hitboxRadius = (int)(0.375 * Game.boardSizeI.x / 8);
