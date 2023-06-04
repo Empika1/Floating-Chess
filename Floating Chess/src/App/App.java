49ac60d7f2515c417341b4aba1bfa37eaf5e5bb0
Empika1 <dravidts1@gmail.com>
2023-06-04 00:10:59 -0400

    cardlayout

--- Floating Chess/src/App/App.java
+++ Floating Chess/src/App/App.java
@@ -8,35 +8,32 @@ import Menu.*;
 public class App {
 
     static JFrame f;
+    static JPanel cards;
     public static void main(String[] args) throws Exception{
         UIManager.setLookAndFeel("com.formdev.flatlaf.FlatDarkLaf");
         JFrame.setDefaultLookAndFeelDecorated(true);
 
         f = new JFrame("Floating Chess");
+        cards = new JPanel(new CardLayout());
+        f.setContentPane(cards);
+        cards.add(new MenuScreen(), "MenuScreen");
+        cards.add(new GameScreen(), "GameScreen");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setResizable(false);
+        f.pack();
         f.setLocationRelativeTo(null);
+        f.setVisible(true);
+
         displayMenuScreen();
     }
 
     public static void displayMenuScreen() {
-        f.invalidate();
-        MenuScreen m = new MenuScreen();
-        f.setContentPane(m);
-        f.pack();
-        f.setLocationRelativeTo(null);
-        f.validate();
-        f.setVisible(true);
+        CardLayout cl = (CardLayout)(cards.getLayout());
+        cl.show(cards, "MenuScreen");
     }
 
     public static void displayGameScreen() {
-        f.invalidate();
-        GameScreen g = new GameScreen();
-        f.setContentPane(g);
-        f.pack();
-        f.setLocationRelativeTo(null);
-        f.validate();
-        f.setVisible(true);
-        g.startGame();
+        CardLayout cl = (CardLayout)(cards.getLayout());
+        cl.show(cards, "GameScreen");
     }
 }
